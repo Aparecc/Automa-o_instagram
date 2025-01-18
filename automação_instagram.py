3a7edfd86095040b51d059254008866598e12c4e
Rafael Aparecido da Silva <raffah.doparaiso@gmail.com>
2025-01-17 22:46:44 -0300

    Add files via upload
    Uma Automação feita em Python, entrando no Instagram em segundos.

---
+++ automação_instagram.py
@@ -0,0 +1,43 @@
+import pyautogui
+
+
+#Pressionar a tecla Windows
+pyautogui.press("win")
+
+pyautogui.PAUSE = 2
+#Digitar Chrome
+pyautogui.write("chrome")
+
+#Pressionar Enter para Entrar no Chrome
+pyautogui.press("enter")
+
+pyautogui.sleep (1)
+
+#Escrever o link do Instagram
+pyautogui.write("https:/www.instagram.com")
+
+#Pressionar o Enter para entrar no Site do Instagram
+pyautogui.press("enter")
+
+pyautogui.sleep (2)
+#Achar Posição do Mouse em cima do Email e Clicar 
+pyautogui.click(x=829, y=314)
+
+pyautogui.PAUSE = 0
+#Colocar o Email ou Nome do Usuário
+pyautogui.write("Digite seu email Aqui")
+
+#Descer Para a barra de Senha
+pyautogui.press("Tab") 
+
+#Inserir a Senha 
+pyautogui.write("Digite sua senha Aqui")
+
+#Dar Um Tab Para pressionar o botão iniciar
+pyautogui.press("Tab")
+pyautogui.press("Tab")
+pyautogui.press("Enter")
+#Passo 1 Abrir a Web
+#Passo 2 Entrar no Site do Instagram 
+#Passo 3 Fazer Login
+#Seguir Usuários
